51f44eb355b7a32b339b89eef9275fe692735aad
aymeric <rambaudaymeric@gmail.com>
2023-12-18 14:49:57 +0100

    rajout button retour menu

--- src/route_page/end.tsx
+++ src/route_page/end.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 function End() {
+  const navig = useNavigate()
 
   return (
     <>
+      <div>
       <p>gagné</p>
+      <button onClick={() => navig("/")}>retour menu</button>
+      </div>
     </>
   )
 }
