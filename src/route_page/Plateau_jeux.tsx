64b7a29a1ab813afe52afb4fb195c826eb0d633d
aymeric <rambaudaymeric@gmail.com>
2023-12-19 09:39:26 +0100

    test de la vibration

--- src/route_page/Plateau_jeux.tsx
+++ src/route_page/Plateau_jeux.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import Kill_div from "../composants/Kill_div"
 
+const son = new Audio("/sound-effects-sonic-rings.mp3")
 const son_attante = new Audio("/musique-dattente.mp3")
 
 function Plateau_jeux() {
@@ -10,10 +10,26 @@ function Plateau_jeux() {
   const [score,SetScore] = useState<number>()
   const [timer,SetTimer] = useState<number>()
   const [ping,SetPing] = useState<number>()
+  const [point,Setpoint] = useState<number>(1) // !!!
+  const [random_top,SetRandom_top] = useState(Math.floor(Math.random()*41).toString())
+  const [random_left,SetRandom_left] = useState(Math.floor(Math.random()*101).toString())
 
-  const recup_value = (value:number) =>{
-    SetScore(value)
+  const ajout_point = useCallback(() => {
+  son.play()
+  SetRandom_top(Math.floor(Math.random()*41).toString())
+  SetRandom_left(Math.floor(Math.random()*101).toString())
+  
+  if (point===10) {
+    son_attante.currentTime = 0
+    son_attante.pause()
+    navig("/end/" + (timer ? (Date.now()-timer) / 1000:0).toString())
+  }else{
+    navigator.vibrate(200)
+    Setpoint(point+1)
+    SetScore(point)
+    son.currentTime = 0
   }
+  },[random_top, random_left, point])
 
   son_attante.play()  
 
@@ -38,7 +54,7 @@ function Plateau_jeux() {
     <>
       <div className="div_plateau_jeu">
 
-        <Kill_div Timer={(timer ? (Date.now()-timer) / 1000:0).toString()} resultat_point={(value) => recup_value(value)}/>
+        <div onClick={ajout_point} className="kill_div" style={{marginTop: random_top+"%", marginLeft: random_left+"%"}}></div>
 
         <div className="div_navig_start">
 
