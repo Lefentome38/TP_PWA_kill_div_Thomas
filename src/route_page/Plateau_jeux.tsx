7eaca85ab81f410eddbb694dd3b8552fc9ebcc2b
aymeric <rambaudaymeric@gmail.com>
2023-12-20 16:22:54 +0100

    BUG correction de localstorage Iphone

--- src/route_page/Plateau_jeux.tsx
+++ src/route_page/Plateau_jeux.tsx
@@ -10,11 +10,16 @@ function Plateau_jeux() {
   const [timer,SetTimer] = useState<number>()
   const [ping,SetPing] = useState<number>()
   const [point,Setpoint] = useState<number>(1)
-  const [random_top,SetRandom_top] = useState(Math.floor(Math.random()*41).toString())
-  const [random_left,SetRandom_left] = useState(Math.floor(Math.random()*101).toString())
+  const [random_top,SetRandom_top] = useState(Math.floor(Math.random()*51).toString())
+  const [random_left,SetRandom_left] = useState(Math.floor(Math.random()*81).toString())
 
   const ajout_point = useCallback(() => {
     son.play()
+
+    function sum_timer() {
+      return (timer ? (Date.now()-timer) / 1000:0).toString()
+    }
+
     if (point===10) {
       son_attante.currentTime = 0
       son_attante.pause()
@@ -23,8 +28,10 @@ function Plateau_jeux() {
       //     new Notification("Hi there!", { body: (timer ? (Date.now()-timer) / 1000:0).toString()});
       //   }
       // });
-      localStorage.setItem(localStorage.length.toString() ,(timer ? (Date.now()-timer) / 1000:0).toString()) // !!!
-      navig("/end/" + (timer ? (Date.now()-timer) / 1000:0).toString())
+      if (localStorage) {
+        localStorage.setItem(localStorage.length.toString() ,sum_timer()) // !!!
+      }
+      navig("/end/" + sum_timer())
     }else{
       if (navigator.vibrate) {
         navigator.vibrate([200]) // !!!
@@ -33,9 +40,8 @@ function Plateau_jeux() {
       son.currentTime = 0
     }
     SetRandom_top(Math.floor(Math.random()*41).toString())
-    SetRandom_left(Math.floor(Math.random()*101).toString())
+    SetRandom_left(Math.floor(Math.random()*81).toString())
   },[random_top, random_left, point])
-
   
   useEffect(() => {
     son_attante.play()  
@@ -57,15 +63,13 @@ function Plateau_jeux() {
   return (
     <>
       <div className="div_plateau_jeu">
-
-        <div onClick={ajout_point} className="kill_div" style={{marginTop: random_top+"%", marginLeft: random_left+"%"}}></div>
-
         <div className="div_navig_start">
-
-          <p className="p_score">{point-1}</p>
-          <p>{(timer ? (Date.now()-timer) / 1000:0)}</p>
           <button onClick={navig_start}>go menu</button>
-
+          <p>{(timer ? (Date.now()-timer) / 1000:0)}</p>
+          <p className="p_score">{point-1}</p>
+        </div>
+        <div className="zone_div_kill">
+           <div onClick={ajout_point} className="kill_div" style={{marginTop: random_top+"%", marginLeft: random_left+"%"}}></div>  {/* problème de placement des div_kill, ils sortent de la zone de jeu */}
         </div>
       </div>
     </>
